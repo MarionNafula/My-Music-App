2bc1d3f410dba6fddde52dcc3ebd52cf923b6383
Marion <wekesamarion100@gmail.com>
2024-11-13 20:12:40 +0300

    setting routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import Header from './components/Header';
 import SongList from './components/SongList';
 import SongDetail from './components/SongDetail';
 import Footer from './components/Footer';
+
+const Home = ({ songs, onSongClick }) => (
+  <div>
+    <SongList songs={songs} onSongClick={onSongClick} />
+  </div>
+);
+
+const About = () => <div><h2>About Us</h2><p>Information about the music website.</p></div>;
+const Genres = () => <div><h2>Genres</h2><p>Explore various music genres.</p></div>;
+const Artists = () => <div><h2>Artists</h2><p>List of popular artists.</p></div>;
 
 const App = () => {
   const [songs, setSongs] = useState([]);
   const [selectedSong, setSelectedSong] = useState(null);
 
   useEffect(() => {
-    // Fetch data from db.json
     axios.get('http://localhost:5000/Songs')
       .then(response => {
         setSongs(response.data);
@@ -26,15 +35,17 @@ const App = () => {
   };
 
   return (
-    <div>
+    <Router>
       <Navbar />
-      {/* <Header /> */}
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <SongList songs={songs} onSongClick={handleSongClick} />
-        {selectedSong && <SongDetail song={selectedSong} />}
-      </div>
+      <Routes>
+        <Route path="/" element={<Home songs={songs} onSongClick={handleSongClick} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/genres" element={<Genres />} />
+        <Route path="/artists" element={<Artists />} />
+      </Routes>
+      {selectedSong && <SongDetail song={selectedSong} />}
       <Footer />
-    </div>
+    </Router>
   );
 };
 
